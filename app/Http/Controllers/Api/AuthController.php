1a7082c13734028d707293de82350970fbd57acf
Juan Esteban Deossa Pertuz <jedp082@gmail.com>
2022-02-22 19:29:04 -0500

    feature: API Auth for SPAs done

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        $validator = Validator::make($request->post(), [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+       
+
+        $validator->validate();
+
+        if($validator->failed()) {
+            return response()->json([
+                "status" => 401,
+                "data" => false,
+                "message" => "Need more data to auth"
+            ], 401);
+        }
+
+        
+        $isLogged = Auth::attempt(["email" => $request->email, "password" => $request->password], true);
+
+        if(!$isLogged) {
+            return response()->json([
+                "status" => 401,
+                "data" => false,
+                "message" => "Bad credentials"
+            ], 401);
+        }
+
+     
+
+        $user = $request->user();
+        $userRole = $user->role()->first(['name']);
+        $tokenData = [
+            "email" => $user->email,
+            "role" => $userRole
+        ];
+        $token = $user->createToken(json_encode($tokenData));
+
+        return [
+            "status" => 200,
+            "data" => [
+                "token" => $token->plainTextToken,
+                "role" => $userRole
+            ],
+            "message" => "Logged in!"
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ Route::get('/', function () {
     ]);
 });
 
+
+
 Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
